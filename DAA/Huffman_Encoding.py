274c3cc1a394b03b59e5fef607f8abb351addfe8
Zaki Shaikh <72220348+NovaBoy04@users.noreply.github.com>
2022-09-27 22:40:02 +0530

    Update Huffman_Encoding.py

--- DAA/Huffman_Encoding.py
+++ DAA/Huffman_Encoding.py
@@ -21,21 +21,7 @@ class node:
 # created Huffman tree
  
  
-def printNodes(node, val=''):
-    # huffman code for current node
-    newVal = val + str(node.huff)
- 
-    # if node is not an edge node
-    # then traverse inside it
-    if(node.left):
-        printNodes(node.left, newVal)
-    if(node.right):
-        printNodes(node.right, newVal)
- 
-        # if node is edge node then
-        # display its huffman code
-    if(not node.left and not node.right):
-        print(f"{node.symbol} -> {newVal}")
+
  
  
 x = str(input("Enter your string "))
